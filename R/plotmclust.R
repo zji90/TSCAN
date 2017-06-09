cab795776bd52d36470056d1c08953761ef0368b
Zhicheng Ji <zhichengji@gmail.com>
2017-06-10 05:31:35 +0800

    add show_full_tree to plotmclust

--- R/plotmclust.R
+++ R/plotmclust.R
@@ -9,12 +9,13 @@
 #' @param y The column of data after dimension reduction to be plotted on the vertical axis.
 #' @param MSTorder The arbitrary order of cluster to be shown on the plot.
 #' @param show_tree Whether to show the links between cells connected in the minimum spanning tree.
+#' @param show_full_tree Whether to show the full tree or not. Only useful when show_tree=T. Overrides MSTorder.
 #' @param show_cell_names Whether to draw the name of each cell in the plot.
 #' @param cell_name_size The size of cell name labels if show_cell_names is TRUE.
 #' @param markerexpr The gene expression used to define the size of nodes.
 #' @return A ggplot2 object.
 #' @export
-#' @import ggplot2 plyr grid
+#' @import ggplot2 plyr grid igraph
 #' @author Zhicheng Ji, Hongkai Ji <zji4@@zji4.edu>
 #' @examples
 #' data(lpsdata)
@@ -22,7 +23,7 @@
 #' lpsmclust <- exprmclust(procdata)
 #' plotmclust(lpsmclust)
 
-plotmclust <- function(mclustobj, x = 1, y = 2, MSTorder = NULL, show_tree = T, show_cell_names = T, cell_name_size = 3, markerexpr = NULL) {
+plotmclust <- function(mclustobj, x = 1, y = 2, MSTorder = NULL, show_tree = T, show_full_tree = F, show_cell_names = T, cell_name_size = 3, markerexpr = NULL) {
       color_by = "State"
       
       lib_info_with_pseudo <- data.frame(State=mclustobj$clusterid,sample_name=names(mclustobj$clusterid))
@@ -47,26 +48,32 @@ plotmclust <- function(mclustobj, x = 1, y = 2, MSTorder = NULL, show_tree = T, 
       if (show_tree) {
             clucenter <- mclustobj$clucenter[,c(x,y)]
             clulines <- NULL
-            if (is.null(MSTorder)) {
-                  clutable <- table(mclustobj$clusterid)
-                  alldeg <- degree(mclustobj$MSTtree)
-                  allcomb <- expand.grid(as.numeric(names(alldeg)[alldeg == 
-                                                                        1]), as.numeric(names(alldeg)[alldeg == 1]))
-                  allcomb <- allcomb[allcomb[, 1] < allcomb[, 2], ]
-                  numres <- t(apply(allcomb, 1, function(i) {
-                        tmp <- as.vector(get.shortest.paths(mclustobj$MSTtree, 
-                                                            i[1], i[2])$vpath[[1]])
-                        c(length(tmp), sum(clutable[tmp]))
-                  }))
-                  optcomb <- allcomb[order(numres[, 1], numres[, 2], decreasing = T)[1], 
-                                     ]
-                  branchcomb <- allcomb[-order(numres[, 1], numres[, 2], 
-                                               decreasing = T)[1], ]
-                  MSTorder <- get.shortest.paths(mclustobj$MSTtree, optcomb[1], 
-                                                 optcomb[2])$vpath[[1]]
-            }            
-            for (i in 1:(length(MSTorder)-1)) {
-                  clulines <- rbind(clulines, c(clucenter[MSTorder[i],],clucenter[MSTorder[i+1],]))
+            if (show_full_tree) {
+                  alledges <- as.data.frame(get.edgelist(mclustobj$MSTtree),stringsAsFactors=F)
+                  alledges[,1] <- as.numeric(alledges[,1])
+                  alledges[,2] <- as.numeric(alledges[,2])
+                  for (i in 1:nrow(alledges)) {
+                        clulines <- rbind(clulines, c(clucenter[alledges[i,1],],clucenter[alledges[i,2],]))
+                  }      
+            } else {
+                  if (is.null(MSTorder)) {
+                        clutable <- table(mclustobj$clusterid)
+                        alldeg <- degree(mclustobj$MSTtree)
+                        allcomb <- expand.grid(as.numeric(names(alldeg)[alldeg == 
+                                                                              1]), as.numeric(names(alldeg)[alldeg == 1]))
+                        allcomb <- allcomb[allcomb[, 1] < allcomb[, 2], ]
+                        numres <- t(apply(allcomb, 1, function(i) {
+                              tmp <- as.vector(get.shortest.paths(mclustobj$MSTtree, 
+                                                                  i[1], i[2])$vpath[[1]])
+                              c(length(tmp), sum(clutable[tmp]))
+                        }))
+                        optcomb <- allcomb[order(numres[, 1], numres[, 2], decreasing = T)[1], ]
+                        MSTorder <- get.shortest.paths(mclustobj$MSTtree, optcomb[1], 
+                                                       optcomb[2])$vpath[[1]]
+                  }
+                  for (i in 1:(length(MSTorder)-1)) {
+                        clulines <- rbind(clulines, c(clucenter[MSTorder[i],],clucenter[MSTorder[i+1],]))
+                  }      
             }
             clulines <- data.frame(x=clulines[,1],xend=clulines[,3],y=clulines[,2],yend=clulines[,4])
             g <- g + geom_segment(aes_string(x="x",xend="xend",y="y",yend="yend",size=NULL),data=clulines,size=1)
